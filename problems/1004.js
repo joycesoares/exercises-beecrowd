211b7129cab5daaec8e3210c1c190c57ef050bd7
joycesoares <joyyceesooares@gmail.com>
2022-12-30 17:57:58 -0300

    Exercise 1004 resolution

---
+++ problems/1004.js
@@ -0,0 +1,10 @@
+export function problem(lines) {
+  // var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+  // var lines = input.split('\n');
+
+  const A = Number(lines[0]);
+  const B = Number(lines[1]);
+  let PROD = 0;
+
+  console.log(`PROD = ${(PROD = A * B)}`);
+}
