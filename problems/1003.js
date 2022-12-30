ca8c3f4fd61ce728e092ed750bc0b0adc931cb01
joycesoares <joyyceesooares@gmail.com>
2022-12-30 17:53:52 -0300

    Exercise 1003 resolution

---
+++ problems/1003.js
@@ -0,0 +1,10 @@
+export function problem(lines) {
+  // var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+  // var lines = input.split('\n');
+
+  const A = Number(lines[0]);
+  const B = Number(lines[1]);
+  let SOMA = 0;
+
+  console.log(`SOMA = ${(SOMA = A + B)}`);
+}
